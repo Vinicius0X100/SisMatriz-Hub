791d14513d1ae90069696d3aecef32526f921d64
Vinicius Aquino <vinicius.aquino@sacratech.com>
2026-07-08 20:36:19 -0300

    update 10 atendimento

--- app/Events/FilaAtualizada.php
+++ app/Events/FilaAtualizada.php
@@ -5,11 +5,11 @@ namespace App\Events;
 use App\Models\AtendimentoFila;
 use Illuminate\Broadcasting\Channel;
 use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
 use Illuminate\Foundation\Events\Dispatchable;
 use Illuminate\Queue\SerializesModels;
 
-class FilaAtualizada implements ShouldBroadcast
+class FilaAtualizada implements ShouldBroadcastNow
 {
     use Dispatchable, InteractsWithSockets, SerializesModels;
 
